cc0f4e1df44450b3284ca18decef02a4e05374a5
Yaroslavcoder <yaroslav152223@gmail.com>
2024-11-07 14:56:18 +0200

    Update prototypes.js

--- prototypes.js
+++ prototypes.js
@@ -20,3 +20,31 @@ const dictionary = Object.create(null);
 dictionary["apple"] = "A sweet fruit";
 dictionary["book"] = "A set of written or printed pages bound with a cover";
 console.log(dictionary.toString); //undefined
+
+//Переписати класи на функції-конструктори та прототипи
+function Animal(name) {
+  this.name = name;
+}
+
+// Додаємо метод speak до прототипу Animal
+Animal.prototype.speak = function() {
+  console.log(`${this.name} makes a noise.`);
+};
+
+// Конструктор-функція для Dog
+function Dog(name) {
+  this.name = name; // Встановлюємо name без виклику Animal
+}
+
+// Наслідуємо прототип Animal
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+// Перевизначаємо метод speak у прототипі Dog
+Dog.prototype.speak = function() {
+  console.log(`${this.name} barks.`);
+};
+
+// Створюємо екземпляр Dog і викликаємо метод speak
+const dog = new Dog('Rex');
+dog.speak(); // Rex barks
